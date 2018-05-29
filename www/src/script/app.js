f88f919b3912756aa3f2698a9685c52df1ea3273
Insight <00insight00@gmail.com>
2018-05-29 08:57:10 +0430

    Update styles and new structures

--- www/src/script/app.js
+++ www/src/script/app.js
@@ -1,21 +1,41 @@
-// global deviceready event
+// global events
 document.addEventListener('deviceready', function() {
-
+    
+}, false);
+document.addEventListener('backbutton', function() {
+    goBack();
 }, false);
 
+// Global variables
 var layer = 1;
 
 // Event listeners 
+$('.learn').click(function() {
+    $('.startPage').fadeOut(300);
+    $('.learnPage').fadeIn(300);
+    layer++;
+});
 $('.about').click(function() {
     $('.startPage').fadeOut(300);
     $('.aboutAppPage').fadeIn(300);
     layer++;
 });
-$('.back').click(function() { 
-    $('.aboutAppPage').fadeOut(400);
-    $('.startPage').fadeIn(400);
-    layer--;
-});
 $('.exit').click(function() { 
     navigator.app.exitApp();
 });
+$('.back').click(function() { 
+    goBack();
+});
+// additional functionality
+function goBack() {
+    switch(layer) {
+        case 1: navigator.app.exitApp();
+        case 2:
+            $('.aboutAppPage').fadeOut(300);
+            $('.learnPage').fadeOut(300);
+            window.setTimeout(function(){$('.startPage').fadeIn(300);}, 200);
+            layer--;
+            break;
+        case 3: break;
+    }
+}
