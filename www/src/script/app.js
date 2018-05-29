8031d3d3ebe012a4dc471a25ba6cb86e29415fe4
Insight <00insight00@gmail.com>
2018-05-29 08:02:44 +0430

    Make AboutPage V1

--- www/src/script/app.js
+++ www/src/script/app.js
@@ -2,3 +2,20 @@
 document.addEventListener('deviceready', function() {
 
 }, false);
+
+var layer = 1;
+
+// Event listeners 
+$('.about').click(function() {
+    $('.startPage').fadeOut(300);
+    $('.aboutAppPage').fadeIn(300);
+    layer++;
+});
+$('.back').click(function() { 
+    $('.aboutAppPage').fadeOut(400);
+    $('.startPage').fadeIn(400);
+    layer--;
+});
+$('.exit').click(function() { 
+    navigator.app.exitApp();
+});
